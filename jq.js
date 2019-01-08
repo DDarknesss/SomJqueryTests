e1da2b6b47254e3b2638b63b1ffdfd6ce36a33bc
Davit <davit@Davits-MacBook-Air.local>
2019-01-08 12:56:23 +0400

    controller added

--- jq.js
+++ jq.js
@@ -1,60 +1,81 @@
-class model{}
-
-
-
-class view{
-     AddView (params) {
-        
-    }
-
-    RemoveView(params){
-
-    }
-
-}
-
 $(document).ready(function() {
     const $input = $('#main-input');
     const $list = $('#list-container');
     const $div = $('div');
+    const $deleteButton = $(`.added-button`);
 
-    function addToList(list) {
-        list.forEach(task => {
-            $list.append( '<div><li id=""><label id="input-label"><input type="checkbox" ' 
-            + (task.checkValue ? 'checked' : '') 
-            + '><span>' + task.userText + '</span><label></li></div>');             
-        });
+    class View {
+        addToList(elements){
+            elements.forEach(task => {
+                $list.append(`
+                <div id="added-div">
+                    <li class="input-line">
+                        <label>
+                            <input type="checkbox" ${(task.checkValue ? 'checked' : '')}>
+                        </label>
+                        <span class="added-span">${task.userText}</span>
+                        <button class="added-button">X</button>
+                    </li>
+                </div>`) 
+            });
+        }
+
+        removeItems() {
+            $list.empty();   
+        }
     }
 
+    class Model {
+        constructor (){
+            this.array = [];
+        }
+        
+        // Need to put out of Model to controller
 
-    const taskList = [
-        {userText: 'ba', checkValue: true},
-        {userText: 'ba', checkValue: true},
-        {userText: 'ba', checkValue: true},
-        {userText: 'ba', checkValue: true},
-        {userText: 'ba', checkValue: true},
-    ]
-    addToList(taskList);
 
-    
-    $input.on('keyup', function keyingUp(e) {
-        let userText = $(this).val();
-        if (userText && e.which == 13) {
-            addToList(userText);
+        addToArray(values){
+            return this.array.push(values);
+        }
+
+        getArray(){
+            return this.array;
+        }
+    }
+
+    class Controller {
+
+        getInputValue() {
+            return $input.val();
+        }
+    }
+
+    const TheView = new View();
+    const TheModel = new Model();
+    const TheController = new Controller();
+
+    $input.on(`keyup`, function keyingUp(e) {
+        if ($input.val()!= "" && e.which == 13) {
+            let userText = TheController.getInputValue();
+            TheModel.addToArray({"userText": userText, "checkValue": false});
+            let Todo =  TheModel.getArray();
             $(this).val('');
+            TheView.removeItems();
+            TheView.addToList(Todo);
         }
     });
 
-    $div.on( 'dblclick', '#input-label', function() {
+    $div.on( `dblclick`, `#input-label`, function() {
         if($(this).text()){ 
             let userText = $(this).text();
-            $(this).replaceWith('<label><input type="text" value="' + userText + '"></label> ')
+            $(this).replaceWith(`<label><input type="text" value=${userText}></label>`)
         }
     } );
 
-    // $mainelement.on('click', function(event) {
-    //     if (event.target !== target.is( "input" )  ){
-    //         alert(event.currentTarget);
-    //     }
-    // });
+    $div.on(`click`, `.added-button`, function(){
+        $(this).parent(`li`).remove();
+    })
+
+    $(`#info-button`).on(`click`, function(){
+        console.log(JSON.stringify(TheController.getInputValue()))
+    })
 });
