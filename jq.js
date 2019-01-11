3c0db3c97879f897089eec87adb8ad7348b5da6b
Davit <davit@Davits-MacBook-Air.local>
2019-01-11 18:19:35 +0400

    One more

--- jq.js
+++ jq.js
@@ -1,81 +1,135 @@
 $(document).ready(function() {
     const $input = $('#main-input');
     const $list = $('#list-container');
-    const $div = $('div');
-    const $deleteButton = $(`.added-button`);
 
     class View {
-        addToList(elements){
+       checkedText(id){
+           $(`#${id}`).children(`.added-span`).css({
+                "color": "grey",
+                "text-decoration": "line-through"
+            });
+       }
+       
+       unchekedText(id){
+            $(`#${id}`).children(`.added-span`).css({
+                "color": "black",
+                "text-decoration" : "none"
+            });
+       }
+
+       hideTaskText(id){
+            $(`#${id}`).children(`.added-span`).hide();
+            $(`#${id}`).children(`#task-edit`).show();
+       }
+
+       showTaskText(id){
+            $(`#${id}`).children(`.hidden`).hide();
+            $(`#${id}`).children(`.added-span`).show();
+       }
+
+       removeItems(){
+            $list.empty();   
+        }
+
+        addItems(elements){
             elements.forEach(task => {
                 $list.append(`
                 <div id="added-div">
-                    <li class="input-line">
-                        <label>
-                            <input type="checkbox" ${(task.checkValue ? 'checked' : '')}>
-                        </label>
+                    <li id="${task.id}" class="input-line">
+                        <input class="check-class checkbox-round" type="checkbox" ${(task.checkValue ? 'checked' : '')}>
                         <span class="added-span">${task.userText}</span>
-                        <button class="added-button">X</button>
+                        <input type="text" id="task-edit" class="hidden" value="${task.userText}">
+                        <button class="delete-button">X</button>
                     </li>
                 </div>`) 
             });
         }
 
-        removeItems() {
-            $list.empty();   
+        UpdateView(Todo){
+            TheView.removeItems();
+            TheView.addItems(Todo);
         }
     }
+
 
     class Model {
         constructor (){
             this.array = [];
+            this.id = 0;
         }
-        
-        // Need to put out of Model to controller
-
 
         addToArray(values){
-            return this.array.push(values);
+            this.array.push({
+                "userText": values,
+                "checkValue": false,
+                "id" : this.id++
+            });
         }
 
+        replaceItem(id, changedText){
+            this.array[id].userText =  changedText;
+        }
+
+        spliceArray(id){
+            this.array.splice(id, 1);
+        }
+
+        changeCheckValue(id){
+            if(this.array[id].checkValue){
+                this.array[id].checkValue = false;
+            }else {
+            this.array[id].checkValue = true;
+            }
+        }
+        
         getArray(){
             return this.array;
         }
     }
 
-    class Controller {
-
-        getInputValue() {
-            return $input.val();
-        }
-    }
-
     const TheView = new View();
     const TheModel = new Model();
-    const TheController = new Controller();
 
     $input.on(`keyup`, function keyingUp(e) {
-        if ($input.val()!= "" && e.which == 13) {
-            let userText = TheController.getInputValue();
-            TheModel.addToArray({"userText": userText, "checkValue": false});
-            let Todo =  TheModel.getArray();
+        let userText = $input.val();
+        if (userText && e.which == 13){
+            TheModel.addToArray(userText);
             $(this).val('');
-            TheView.removeItems();
-            TheView.addToList(Todo);
+            TheView.UpdateView(TheModel.getArray());
         }
     });
 
-    $div.on( `dblclick`, `#input-label`, function() {
-        if($(this).text()){ 
-            let userText = $(this).text();
-            $(this).replaceWith(`<label><input type="text" value=${userText}></label>`)
-        }
+    $list.on(`click`, `.delete-button`, function(){
+        let id = $(this).parent(`li`).attr(`id`);
+        TheModel.spliceArray(id);
+        TheView.UpdateView(TheModel.getArray());
+    });
+
+// EDITING TASK 
+    $list.on( `dblclick`, `.input-line`, function() {
+        let id = $(this).attr(`id`);
+        TheView.hideTaskText(id);
     } );
 
-    $div.on(`click`, `.added-button`, function(){
-        $(this).parent(`li`).remove();
-    })
 
-    $(`#info-button`).on(`click`, function(){
-        console.log(JSON.stringify(TheController.getInputValue()))
+    $list.on(`keyup`, `.hidden`, function keyingUp(e) {
+        let id = $(this).parent(`li`).attr(`id`);
+        let userText =  $(`#${id}`).children(`.hidden`).val();
+        if (userText && e.which == 13) {
+            TheView.showTaskText(id);
+            TheModel.replaceItem(id, userText);
+            TheView.UpdateView(TheModel.getArray());
+        }
+    });
+
+// Check
+
+    $list.on(`click`, `.check-class`, function(){
+        let id = $(this).parent(`li`).attr(`id`);
+        if($(this).is(':checked')){
+            TheView.checkedText(id);
+        } else {
+            TheView.unchekedText(id);
+        }    
     })
 });
